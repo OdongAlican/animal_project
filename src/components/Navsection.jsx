4138e2ff12233c0603b8c924af0989296cc6b58d
OdongAlican <sandieo.2020@gmail.com>
2020-11-27 01:58:24 +0300

    update navbar section

--- src/components/Navsection.jsx
+++ src/components/Navsection.jsx
@@ -1,8 +1,29 @@
-import {React, useState} from 'react'
-import { Link, withRouter } from 'react-router-dom'
-import Logo from '../Logo/animal.jpeg'
+import {React, useState, useEffect} from 'react'
+import { withRouter, NavLink } from 'react-router-dom'
+import Logo from '../Logo/animalcircle.png'
 
 const NavBar = ({ history, text, handleInput }) => {
+
+    const [hiddenNav, sethiddenNav] = useState("")
+    
+    const hideNav = () => {
+        if( window.innerWidth < 1076 &&  hiddenNav==="d-none"){
+            sethiddenNav("")
+        }else{
+            sethiddenNav("d-none")
+        }
+    }
+
+    const handleResize = () => {
+        if(window.innerWidth > 1040 ) {
+            sethiddenNav("")
+        }
+      }
+
+    useEffect(()=> {
+        window.addEventListener("resize", handleResize);
+    }, [])
+
 
     const handleChange = (value) => {
         history.push(`/${value}`)
@@ -13,12 +34,13 @@ const NavBar = ({ history, text, handleInput }) => {
             <div className="top-nav-section d-flex">
                 <div className="left-nav"></div>
                 <div className="central-nav">
-                    <div className="nav-title w-75 p-3 d-flex">
+                    <div className="nav-title w-75 p-3 d-flex justify-content-center">
                         <div className="company-logo">
                             <img className="logo-image-style" src={ Logo } alt="logo"></img>
                         </div>
-                        <div>
-                            <p>Animal Initiative</p>
+                        <div className="main-page-header">
+                            <h5>Welcome to Animal Rescue Initiative</h5>
+                            <span className="p-0">Compasionate care</span>
                         </div>
                     </div>
                     <div className="nav-icons d-flex">
@@ -42,19 +64,30 @@ const NavBar = ({ history, text, handleInput }) => {
                 <div className="right-nav"></div>
             </div>
             <div className="upper-nav-section">
-                <div className="d-flex">
-                    <Link className="p-3" to={{
+                <div className="drop-down-icon">
+                    <i onClick= { hideNav } class="fas fa-bars text-white"></i>
+                </div>
+                <div className={`various-screen-nav ${hiddenNav}`}>
+                    <NavLink 
+                    activeClassName="active-button"
+                    className="p-3" exact to={{
                         pathname: '/'
-                    }}> HOME </Link> 
-                    <Link className="p-3" to={{
+                    }}> HOME </NavLink> 
+                    <NavLink 
+                    activeClassName="active-button"
+                    className="p-3" to={{
                         pathname: '/about'
-                    }}> ABOUT US </Link> 
-                    <Link className="p-3" to={{
+                    }}> ABOUT US </NavLink> 
+                    <NavLink 
+                    activeClassName="active-button"
+                    className="p-3" to={{
                         pathname: '/contact'
-                    }}> CONTACT US </Link> 
-                    <Link className="p-3" to={{
+                    }}> CONTACT US </NavLink> 
+                    <NavLink 
+                    activeClassName="active-button"
+                    className="p-3" to={{
                         pathname: '/donations'
-                    }}> DONATIONS </Link> 
+                    }}> DONATIONS </NavLink> 
                     <select onChange={event => handleChange(event.target.value)}>
                         <option value="" disabled selected hidden>PARTNERSHIPS</option>
                         <option value="partnership">PARTNERSHIPS</option>
@@ -64,20 +97,22 @@ const NavBar = ({ history, text, handleInput }) => {
                         <option value="members">MEMBERS/MEMBERSHIP</option>
                         <option value="schools">SCHOOLS</option>
                     </select>
-                    <Link className="p-3" to={{
+                    <NavLink 
+                    activeClassName="active-button"
+                    className="p-3" to={{
                         pathname: '/gallary'
-                    }}> GALLARY </Link> 
+                    }}> GALLARY </NavLink> 
                 </div>
                 <div className="search-section">
-                <div className="nav-search">
-                    <input 
-                    value= { text } 
-                    type="search" 
-                    name="nav-search" 
-                    onChange={handleInput}
-                    placeholder="Search Word"/>
-                    <i className="fas fa-search nav-fa-search"></i>
-                </div>
+                    <div className="nav-search">
+                        <input 
+                        value= { text } 
+                        type="search" 
+                        name="nav-search" 
+                        onChange={handleInput}
+                        placeholder="Search"/>
+                        <i className="fas fa-search nav-fa-search"></i>
+                    </div>
                 </div>
             </div>
         </div>
